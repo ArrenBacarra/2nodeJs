2f62e4d01daa2cf715b1acdd8028b23ce174267c
ArrenBacarra <arrenbacarra442@gmail.com>
2024-09-21 09:32:39 +0800

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -6,7 +6,11 @@ router.get('/about', main.about);
 router.get('/contact', main.contact);
 router.get('/info', main.info);
 router.get('/settings', main.settings);
-
+router.get('/kikiam', main.kikiam);
+router.get('/pisbol', main.pisbol);
+router.get('/bananaq', main.bananaq);
+router.get('/hotdog', main.hotdog);
+router.get('/footlong', main.footlong);
 
 
 module.exports = router;
